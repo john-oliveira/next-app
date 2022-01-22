f3ea775955b756816b474b916a2e6b91d1196661
John, Oliveira <elton.pmp@gmail.com>
2022-01-22 11:27:40 -0300

    Add Edit Page

--- pages/api/usuarios/dados.ts
+++ pages/api/usuarios/dados.ts
@@ -1,8 +1,12 @@
 import Usuario from "../../../model/Usuario";
 
-export const usuarios: Usuario[] = [
-    {id: '1', name: 'João', username: 'jo', email: 'jo@gmail.com'},
-    {id: '2', name: 'Maria', username: 'ma', email: 'ma@gmail.com'},
-    {id: '3', name: 'Silvia', username: 'si', email: 'si@gmail.com'},
-    {id: '4', name: 'Rosa', username: 'ro', email: 'ro@gmail.com'}
-];
+export let usuarios: Usuario[];
+
+if (!usuarios!) {
+    usuarios = [
+        { id: '1', name: 'João', username: 'jo', email: 'jo@gmail.com' },
+        { id: '2', name: 'Maria', username: 'ma', email: 'ma@gmail.com' },
+        { id: '3', name: 'Silvia', username: 'si', email: 'si@gmail.com' },
+        { id: '4', name: 'Rosa', username: 'ro', email: 'ro@gmail.com' }
+    ];
+}
---
+++ pages/usuarios/edit/[id].tsx
@@ -0,0 +1,64 @@
+import { GetServerSideProps, NextPage } from "next"
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Usuario from "../../../model/Usuario"
+
+const UsuarioDetailPage: NextPage<Props> = ({ usuario }) => {
+  const [name, setName] = useState(usuario.name);
+  const [username, setUsername] = useState(usuario.username);
+  const [email, setEmail] = useState(usuario.email);
+
+  const router = useRouter();
+  
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    usuario.name = name;
+    usuario.username = username;
+    usuario.email = email;
+    const res = await fetch(`http://localhost:3000/api/usuarios/add`, {
+      method: 'POST',
+      body: JSON.stringify(usuario)
+    })
+
+    if(res.status == 200){
+      router.push('/usuarios');
+    }
+  }
+
+  return (
+    <>
+      <h3>Edição do Usuário</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label><br />
+        <input type="text" value={name} onChange={e=>{setName(e.target.value)}}/><br />
+        <label>Username:</label><br />
+        <input type="text" value={username} onChange={e=>{setUsername(e.target.value)}}/><br />
+        <label>Email:</label><br />
+        <input type="email" value={email} onChange={e=>{setEmail(e.target.value)}}/>
+        <button type="submit">Salvar</button>
+      </form>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<Props, Params> = async (context) => {
+  const { id } = context.params! // Non-null assertion operator;
+  //const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  const res = await fetch(`http://localhost:3000/api/usuarios/${id}`)
+  const usuario: Usuario = await res.json()
+  return {
+    props: {
+      usuario
+    }
+  }
+}
+
+type Props = {
+  usuario: Usuario;
+}
+
+type Params = {
+  id: string;
+}
+
+export default UsuarioDetailPage;
