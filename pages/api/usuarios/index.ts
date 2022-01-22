b4f057b564541dc3fc44f02f7e50543b4ba6e698
John, Oliveira <elton.pmp@gmail.com>
2022-01-22 14:12:59 -0300

    Remove api usuários

--- pages/api/usuarios/index.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import Usuario from '../../../model/Usuario'
-import { usuarios } from './dados';
-
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<Usuario[]>) {
-  res.status(200).json(usuarios);
-}
